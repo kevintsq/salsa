66e5c9ec7c65c63c41784148336989ab9e4b031f
Kevin Tan <tan.k@nyu.edu>
2025-04-11 22:28:35 -0400

    add bash

---
+++ stage1.sh
@@ -0,0 +1,14 @@
+python -m experiments.ex_dcase24 with \
+data_loader.batch_size=16 \
+data_loader.batch_size_eval=16 \
+audio_features.segment_length=10 \
+audio_features.model=mn40_as_ext \
+sentence_features.model=roberta-large \
+rampdown_type=cosine \
+max_epochs=20 \
+rampdown_stop=15 \
+warmup_length=1 \
+rampdown_start=1 \
+train_on=clothov2 \
+seed=409194 \
+directories.data_dir=/mnt/SSD1/st5265
